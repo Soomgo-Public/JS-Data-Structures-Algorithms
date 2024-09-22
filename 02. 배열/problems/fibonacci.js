2403697d6e1f68dc6982ad4968457e69bf0df773
wonjin-dev <fsdjin159@gmail.com>
2024-09-22 17:14:17 +0900

    02. 배열 - 문제 추가

---
+++ 02. 배열/problems/fibonacci.js
@@ -0,0 +1,10 @@
+/** 문제
+ * 입력받은 길이 만큼의 피보나찌 수열을 만드세요.
+ * */
+
+let fibonacci = [];
+function createFibonacci() {
+	console.log(fibonacci);
+}
+
+createFibonacci();
---
+++ 02. 배열/problems/matrix.js
@@ -0,0 +1,11 @@
+/** 문제
+ * 1. 3*3 행렬을 만들세요.
+ * 2. i(행), j(열), z(깊이)를 할당하세요.
+ * */
+
+let matrix = [];
+function createMatrix() {
+	console.log(matrix);
+}
+
+createMatrix();
